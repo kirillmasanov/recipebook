fbe034d1157db3ce8e3dcaca1d8adcd4fcd12b38
kirill_masanov <kirill_masanov@mail.ru>
2020-08-15 01:12:33 +0300

    create GET and POST methods with RecipeResource class - get method, for getting back a single recipe; and the put method, for updating the recipe

--- resources/recipe.py
+++ resources/recipe.py
@@ -23,3 +23,24 @@ class RecipeListResource(Resource):
                         directions=data['directions'])
         recipe_list.append(recipe)
         return recipe.data, HTTPStatus.CREATED
+
+
+class RecipeResource(Resource):
+
+    def get(self, recipe_id):
+        recipe = next((recipe for recipe in recipe_list if recipe_id == recipe_id and recipe.is_publish == True), None)
+        if recipe is None:
+            return {'message': 'recipe not found'}, HTTPStatus.NOT_FOUND
+        return recipe.data, HTTPStatus.OK
+
+    def put(self, recipe_id):
+        data = request.get_json()
+        recipe = next((recipe for recipe in recipe_list if recipe_id == recipe_id), None)
+        if recipe is None:
+            return {'message': 'recipe not found'}, HTTPStatus.NOT_FOUND
+        recipe.name = data['name']
+        recipe.description = data['description']
+        recipe.num_of_servings = data['num_of_servings']
+        recipe.cook_time = data['cook_time']
+        recipe.directions = data['directions']
+        return recipe.data, HTTPStatus.OK
